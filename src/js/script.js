2548c522523d90633db0f07aeb8af063ff1507ef
jeneziss <103391959+jeneziss@users.noreply.github.com>
2024-03-13 23:33:10 +0200

    alpha test vesion 1
    added: news page to add slider for univ lesson

--- src/js/script.js
+++ src/js/script.js
@@ -25,6 +25,35 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 
+
+// Slider //
+let slideIndex = 1;
+document.addEventListener("DOMContentLoaded", function() {
+    showSlides(slideIndex);
+});
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('show');
+    }
+
+    if(slides.length > 0) {
+        slides[slideIndex - 1].classList.add('show');
+    }
+}
 // Modal //
 
 
