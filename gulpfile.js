43ed5c9dae9d4da0ecb177789d826cda411c12c3
jeneziss <103391959+jeneziss@users.noreply.github.com>
2024-03-14 15:35:52 +0200

    alpha test version 2
    upd:
    gulp file fixed a bit .scss>css converter

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass')); // Assuming older Node.js version
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
+const concat = require('gulp-concat');
 
 gulp.task('server', function() {
 
@@ -15,21 +16,22 @@ gulp.task('server', function() {
   gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('styles', async function() {
+gulp.task('style', async function() {
   const autoprefixerModule = await import('gulp-autoprefixer');
   const autoprefixer = autoprefixerModule.default;
 
   return gulp.src("src/sass/**/*.+(scss|sass)")
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(rename({suffix: '.min', prefix: ''}))
-    .pipe(autoprefixer()) // Use the imported autoprefixer
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest("src/css"))
-    .pipe(browserSync.stream());
+      .pipe(concat('style.scss')) // Concatenate all SCSS files into one
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(rename({suffix: '.min', prefix: ''}))
+      .pipe(autoprefixer()) // Use the imported autoprefixer
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(gulp.dest("src/css"))
+      .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-  gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+  gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('style'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('default', gulp.parallel('watch', 'server', 'style'));
