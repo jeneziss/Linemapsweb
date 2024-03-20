2861eb39e90ea5dc6b8391d34727b2ddea3deeb1
jeneziss <103391959+jeneziss@users.noreply.github.com>
2024-03-20 14:28:11 +0200

    fixing

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create(); // Create a browserSync instance
-const sass = require('gulp-sass')(require('sass'));
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer'); // Import autoprefixer directly
 
 gulp.task('server', function() {
-  browserSync.init({
+
+  browserSync({
     server: {
       baseDir: "src"
     }
@@ -16,19 +16,18 @@ gulp.task('server', function() {
   gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('style', function() {
-  return gulp.src("src/sass/**/*.+(scss|sass)")
-      .pipe(concat('style.scss'))
-      .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-      .pipe(rename({ suffix: '.min', prefix: '' }))
-      .pipe(autoprefixer()) // Use autoprefixer directly
-      .pipe(cleanCSS({ compatibility: 'ie8' }))
+gulp.task('styles', function() {
+  return gulp.src("src/sass/style.+(scss|sass)")
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(rename({suffix: '.min', prefix: ''}))
+      .pipe(autoprefixer())
+      .pipe(cleanCSS({compatibility: 'ie8'}))
       .pipe(gulp.dest("src/css"))
       .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-  gulp.watch("src/sass/**/*.+(scss|sass)", gulp.series('style')); // Use gulp.series for proper task chaining
-});
+  gulp.watch("src/sass/style.+(scss|sass)", gulp.parallel('styles'));
+})
 
-gulp.task('default', gulp.parallel('watch', 'server', 'style')); // Use gulp.parallel for proper task chaining
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
